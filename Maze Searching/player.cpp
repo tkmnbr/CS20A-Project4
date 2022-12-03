2c6f5af4bd634e81bae0d549f43380072f443bf9
Noboru Tokimi <t.nbr0304@gmail.com>
2022-12-02 20:42:42 -0800

    added backtracking

--- Maze Searching/player.cpp
+++ Maze Searching/player.cpp
@@ -108,28 +108,79 @@ void Player::say() {
 //		Backtracking is challenging, save it for the very very very last thing.
 //		Make sure the STATE::LOOK aspect compiles and works first.
 void Player::update() {
-
+	// Backtracking enabled
 	if (BACKTRACKENABLED) {
-		
+		//Case for NOEXIT
+		if (m_lookingPaper.empty()) {
+			state(State::NOEXIT);
+			return;
+		}		
+		m_btQueue.enqueue(room());
+		double n = sqrDist(room(), getTargetRoom());
+		if (n > 1.0) {
+			state(State::BACKTRACK);
+			move(m_btList.front());
+			m_btList.pop_front();
+			//m_btStack.push(room());
+			//m_btQueue.enqueue(room());
+			return;
+		}
+		/*while (!m_btStack.empty()) {
+			m_btList.push_front(m_btStack.top());
+			m_btStack.pop();
+		}*/
+		while (!m_btQueue.empty()) {
+			m_btList.push_front(m_btQueue.front());
+			m_btQueue.dequeue();
+		}
+		//LOOK behavier
+		Room pos = m_lookingPaper.front();
+		if (maze()->foundExit(pos)) {
+			state(State::EXIT);
+			return;
+		}
+		m_lookingPaper.dequeue();
+		move(pos);
+
+		Room west = Room(pos.x() - 1, pos.y());
+		Room east = Room(pos.x() + 1, pos.y());
+		Room north = Room(pos.x(), pos.y() - 1);
+		Room south = Room(pos.x(), pos.y() + 1);
+
+		if (maze()->open(west) and !discovered(west)) {
+			m_lookingPaper.enqueue(west);
+			m_discoveredRooms.push_front(west);
+		}
+
+		if (maze()->open(east) and !discovered(east)) {
+			m_lookingPaper.enqueue(east);
+			m_discoveredRooms.push_front(east);
+		}
+
+		if (maze()->open(north) and !discovered(north)) {
+			m_lookingPaper.enqueue(north);
+			m_discoveredRooms.push_front(north);
+		}
+
+		if (maze()->open(south) and !discovered(south)) {
+			m_lookingPaper.enqueue(south);
+			m_discoveredRooms.push_front(south);
+		}
+
 	}
 	else {
-		//get the current pos
-		//mark it as discovered
-		//record curr pos 
-		//m_lookingPaper.dequeu()
-		//if next room is exit, then state EXIT
-
-		//Room west(x-1, y)
-		//Room east(x+1, y)
-		//Room north(x, y-1)
-		//Room south(x, y+1)
-
-		//if west is open and not discovered, m_lookingPaper.enqueue(west) and m_discoveredRoom.pushfront(west)
-		//...
+		//If there is no space to move -> stuck return;
 		if (m_lookingPaper.empty()) {
 			state(State::NOEXIT);
 			return;
 		}
+		//READ FROM THE BOTTOM
+		//get current pos.
+		//if(pos == exit), then state EXIT. return;
+		//m_lookingPaper.dequeue();
+		//set current position to the pos.
+		//Begin algorithm same as lecture using queue
+
 		Room pos = m_lookingPaper.front();
 		if (maze()->foundExit(pos)) {
 			state(State::EXIT);
@@ -164,7 +215,4 @@ void Player::update() {
 		}
 
 	}
-
-
-	
 }
